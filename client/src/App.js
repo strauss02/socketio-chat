6b98c5afafb5d3f15273ca4b91b90f5d6ec5946d
Ido Strauss <strauss02@hotmail.co.il>
2021-12-29 15:17:44 +0200

    implement room join socket event

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import io from 'socket.io-client'
 
 import './App.css'
 const socket = io.connect('http://localhost:3001')
 
 function App() {
-  return <div className="App"></div>
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
+
+  const joinRoom = () => {
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', room)
+    }
+  }
+
+  return (
+    <div className="App">
+      <h3>Join Chat</h3>
+      <input
+        onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="john..."
+      />
+      <input onChange={(e) => setRoom(e.target.value)} placeholder="room id" />
+      <button onClick={joinRoom}>Join a room</button>
+    </div>
+  )
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('user connected', socket.id)
 
+  socket.on('join_room', (data) => {
+    socket.join(data)
+    console.log(`user with id ${socket.id} joined the room ${data}`)
+  })
+
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id)
   })
