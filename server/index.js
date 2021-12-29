3e57ebd3ac07bdab4f189c40badf14eb8528d34c
Ido Strauss <strauss02@hotmail.co.il>
2021-12-29 14:10:38 +0200

    basic setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const { Server } = require('socket.io')
+
+app.use(cors())
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'https://localhost:3000',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id)
+  })
+})
+
+server.listen(3001, () => {
+  console.log('Server Runnig')
+})
